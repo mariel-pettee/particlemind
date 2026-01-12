3cf9198e0f2aec5e962f0264206afaf9ba817bb5
Mariel Pettee <mariel.pettee@gmail.com>
2026-01-11 20:03:14 -0800

    turned VQ-VAE script into a slurm submission script

--- scripts/3_train_vqvae.sh
+++ scripts/3_train_vqvae.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
+#SBATCH -A m4474_g
+#SBATCH -C gpu
+#SBATCH -q regular
+#SBATCH -n 1
+#SBATCH -c 32
+#SBATCH --ntasks=4                   # Number of tasks (4 commands)
+#SBATCH --gpus-per-task=1
+#SBATCH --time 6:00:00
+#SBATCH --output log/%j.log
+#SBATCH --job-name vqvae200
 
 ### go to the right directory relative to this script's location
 PROJECT_ROOT="/global/cfs/cdirs/m3246/mpettee/hep/particlemind"
 
-uv run python $PROJECT_ROOT/src/particlemind/training/train_vqvae.py --train_embedder
+cd $PROJECT_ROOT
+
+uv run python $PROJECT_ROOT/src/particlemind/training/train_vqvae.py --train_embedder --max_epochs 200
